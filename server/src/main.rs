af7155108d9b3ca1a2bde5a0d51892c0c69edbb6
Mqlvin <pricklydotcactus@gmail.com>
2025-06-01 12:29:46 +0100

    change dir serve to reflect server

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,7 +7,7 @@ use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().fallback_service(ServeDir::new("frontend/dist/").append_index_html_on_directories(true));
+    let app = Router::new().fallback_service(ServeDir::new("dist/").append_index_html_on_directories(true));
     let binding = format!("{}:{}", "0.0.0.0", 3000);
 
     // Make the TcpListener, if that is successful, try `serve()` the Axum app
